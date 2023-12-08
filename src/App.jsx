c4608333f89f267c981d65e3b5757c64b5d2608d
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-08 21:44:08 +0600

    route update

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
           <Route path="*" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
           <Route path="/" element={<Navigate to="/" />} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
         <Footer />
       </div>
