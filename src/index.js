baeadc719d049aaaca6efaf85cf9ef8ada91a122
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-12-22 16:16:31 +0600

    routeproblem

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
