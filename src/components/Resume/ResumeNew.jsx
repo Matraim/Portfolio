a56ca4c3b16473b1637d5590c4ef66a411eca7b3
Bismillah <matraim.kg@gmail.com>
2024-01-20 16:57:55 +0600

    added pdf img

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -1,45 +1,38 @@
-import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Particle from '../Particle';
 import { AiOutlineDownload } from 'react-icons/ai';
-import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import pdf from '../../Assets/Projects/MatraimNurmatovResume.pdf';
+import { saveAs } from 'file-saver';
+import imgPdf from '../../Assets/resumeMatraimNurmatov.png';
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+  const handleDownload = () => {
+    saveAs(pdf, 'NurmatovMatraimResume');
+  };
 
   return (
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
+        <img
+          src={imgPdf}
+          alt="Matraim Nurmatov Resume"
+          style={{ maxWidth: '30rem' }}
+        />
+
+        <Row
+          style={{
+            justifyContent: 'center',
+            position: 'relative',
+            margin: '1rem',
+          }}
+        >
           <Button
             variant="primary"
-            target="_blank"
             style={{ maxWidth: '250px' }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: 'center', position: 'relative' }}>
-          <Button
-            variant="primary"
-            target="_blank"
-            style={{ maxWidth: '250px' }}
+            onClick={handleDownload}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
